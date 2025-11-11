15b0e33596dc30ffebd2768fc3ba1e991f206ce6
SoloGudLyf <solomonteiko0@gmail.com>
2025-11-11 03:32:12 +0000

    feat: Create logic for computer to randomly shoot spots

--- src/battleship-ui.js
+++ src/battleship-ui.js
@@ -8,9 +8,10 @@ let state = "player";
 
 function generateGrid(board, arr) {
   const container = document.createElement("div");
+
   container.style.width = "200px";
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[1].length; j++) {
       const div = document.createElement("div");
 
       if (typeof arr[i][j] === "object") {
@@ -43,28 +44,6 @@ function shot(e) {
   gridIndex = [Number(gridIndex[0]), Number(gridIndex[1])];
   if (state === "player") {
     if (
-      !playerBoard.childNodes[1].contains(e.target) ||
-      (e.target.classList.value !== "" && e.target.classList.value !== "ship")
-    )
-      return;
-    if (
-      typeof player.gameBoard.gameboardArr[gridIndex[0]][gridIndex[1]] ===
-      "object"
-    ) {
-      e.target.classList.add("hit");
-    } else {
-      e.target.classList.add("missed");
-      e.target.textContent = ".";
-    }
-
-    player.gameBoard.receiveAttack(gridIndex);
-
-    state = "computer";
-    info.textContent = "It's computer's turn";
-    return;
-  }
-  if (state === "computer") {
-    if (
       !computerBoard.childNodes[3].contains(e.target) ||
       (e.target.classList.value !== "" && e.target.classList.value !== "ship")
     )
@@ -74,7 +53,6 @@ function shot(e) {
       "object"
     ) {
       e.target.classList.add("hit");
-      info.textContent = "It's computer's turn";
     } else {
       e.target.classList.add("missed");
       e.target.textContent = ".";
@@ -82,8 +60,54 @@ function shot(e) {
 
     computer.gameBoard.receiveAttack(gridIndex);
 
+    state = "computer";
+    info.textContent = "It's computer's turn";
+    return;
+  }
+  if (state === "computer") {
+    if (
+      !playerBoard.childNodes[1].contains(e.target) ||
+      (e.target.classList.value !== "" && e.target.classList.value !== "ship")
+    )
+      return;
+    computerChoice();
+
     state = "player";
     info.textContent = "It's player's turn";
     return;
   }
 }
+
+function computerChoice() {
+  let choice = Math.floor(Math.random() * 99) + 1;
+  (document.querySelector(`.playerBoard div :nth-child(100)`));
+
+  let element = document.querySelector(
+    `.playerBoard div :nth-child(${choice})`,
+  );
+  while (element.classList.value !== "" && element.classList.value !== "ship") {
+    choice = Math.floor(Math.random() * 99) + 1;
+
+
+    element = document.querySelector(`.playerBoard div :nth-child(${choice})`);
+  }
+
+  let choiceArr = Array.from(String(choice - 1));
+  choiceArr = [Number(choiceArr[0]), Number(choiceArr[1])];
+
+  choiceArr[1] = Number.isNaN(choiceArr[1]) ? 0 : choiceArr[1];
+
+  if (
+    typeof player.gameBoard.gameboardArr[choiceArr[0]][choiceArr[1]] ===
+    "object"
+  ) {
+
+    element.classList.add("hit");
+  } else {
+    element.classList.add("missed");
+    element.textContent = ".";
+  }
+
+  player.gameBoard.receiveAttack(choiceArr);
+}
+computerChoice();
